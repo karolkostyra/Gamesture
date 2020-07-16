cabd9ee3df9c4bdf7b3c1929d924cb41b717b442
Karol <karol.m.kostyra@gmail.com>
2020-07-16 21:59:50 +0200

    Popup animation

---
+++ Gamesture/Assets/Scripts/PopupAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator popupAnimator;
+
+
+    public void ShowPopupAnim()
+    {
+        popupAnimator.SetTrigger("ShowPopupAnimation");
+        //popupAnimator.SetBool("RunPopupAnimation", true);
+    }
+
+    public void HidePopupAnim()
+    {
+        popupAnimator.SetTrigger("HidePopupAnimation");
+        //popupAnimator.SetBool("RunPopupAnimation", false);
+    }
+}
